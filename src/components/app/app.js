6cc088d29e5efc158edcc639346e094198e190c9
Anna <anna47297@gmail.com>
2023-01-08 21:22:03 +0300

    fix: correct function

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,14 +7,30 @@ const productsContainer = document.querySelector('.products-list');
 const filterBtns = document.querySelectorAll('.list-item');
 
 
-
-// productsContainer.innerHTML = ret cards.forEach(item => {
-//     Card(item)
-// });
-
 productsContainer.innerHTML = (cards.map(item => Card(item))).join("");
 
+let NumberMin = 0;
+let NumberMax = 1000;
+let extraCards = [...cards];
+let extraCardsAllFilter = [...cards];
 
+function getNumberFilter() {
+    NumberMin = priceInputs[0].value
+    NumberMax = priceInputs[1].value
+    // console.log(NumberMin)
+    // console.log(NumberMax)
+
+}
+
+let StockMin = 0;
+let StockMax = 45;
+function getNumberStock() {
+    StockMin = priceInputs[2].value
+    StockMax = priceInputs[3].value
+    console.log(StockMin)
+    console.log(StockMax)
+
+}
 
 //клик по кнопке-> coртировка
 
@@ -24,8 +40,8 @@ productsContainer.innerHTML = (cards.map(item => Card(item))).join("");
 
 let Brand = [];
 let Category = [];
+let nameFilter = [];
 function getNameFIlter(){
-    let nameFilter = [];
         for (let btn of filterBtns) {
             btn.addEventListener('click', (event) => {
             btn.toggleAttribute("disabled")
@@ -44,24 +60,6 @@ function getNameFIlter(){
 
 };
 getNameFIlter();
-
-let extraCards = [...cards];
-let extraCardsAllFilter = [...cards];
-
-function NewFiltArr(arr1, params) {
-    productsContainer.innerHTML = null;
-    if (Brand.length == 0 || Category.length == 0) {
-        extraCards = arr1.filter(item => params.includes(item.brand)  ||  params.includes(item.category))
-        productsContainer.innerHTML = (extraCards.map(item => Card(item))).join("");
-    } else {
-        extraCardsAllFilter = arr1.filter(item => params.includes(item.brand)  &&  params.includes(item.category))
-        productsContainer.innerHTML = (extraCardsAllFilter.map(item => Card(item))).join("");
-    }
-
-}
-
-
-
 
 
     /*let nameFilter = []
@@ -115,8 +113,8 @@ function NewFiltArr(arr1, params) {
 }
 inputFilter();*/
 
-
-
+/////// числовой фильтр
+const priceInputs = document.querySelectorAll(".price-input input");
 const slider = (element) => {
 
     const slider = Boolean(element.classList) ? element : document.querySelector(selector);
@@ -124,13 +122,20 @@ const slider = (element) => {
     const priceInput = slider.querySelectorAll(".price-input input");
     const range = slider.querySelector(".slider .progress");
 
-    let priceGap = 1000;
+    let priceGap = 10;
 
+    //слушатель на инпут
     priceInput.forEach(input => {
         input.addEventListener("input", e => {
+            getNumberFilter();
+            getNumberStock();
+            NewFiltArrNumb(cards, NumberMin, NumberMax);
+            NewFiltArrStock(cards, StockMin, StockMax);
+            // console.log(priceInput[0].value)
+            // console.log(priceInput[1].value)
+
             let minPrice = parseInt(priceInput[0].value),
                 maxPrice = parseInt(priceInput[1].value);
-
             if ((maxPrice - minPrice >= priceGap) && maxPrice <= rangeInput[1].max) {
                 if (e.target.className === "input-min") {
                     rangeInput[0].value = minPrice;
@@ -143,8 +148,14 @@ const slider = (element) => {
         });
     });
 
+
+    //слушатель на ползунок
     rangeInput.forEach(input => {
         input.addEventListener("input", e => {
+            getNumberFilter();
+            getNumberStock();
+            NewFiltArrNumb(cards, NumberMin, NumberMax);
+            NewFiltArrStock(cards, StockMin, StockMax)
             let minVal = parseInt(rangeInput[0].value),
                 maxVal = parseInt(rangeInput[1].value);
             if ((maxVal - minVal) < priceGap) {
@@ -164,3 +175,49 @@ const slider = (element) => {
 }
 
 document.querySelectorAll(".element").forEach(n => slider(n));
+
+
+// const inputNumber = document.querySelector('.filter-price')
+// console.log(inputNumber)
+
+// inputNumber.addEventListener("input", console.log(inputNumber.value))
+
+
+// let NumberMin = 0;
+// let NumberMax = 1000;
+
+
+// let extraCards = [...cards];
+// let extraCardsAllFilter = [...cards];
+
+function NewFiltArr(arr1, params) {
+    console.log(NumberMin)
+    console.log(NumberMax)
+    productsContainer.innerHTML = null;
+    if (Brand.length == 0 || Category.length == 0) {
+        extraCards = arr1.filter(item => item.price >= NumberMin && item.price <= NumberMax && item.stock >= StockMin && item.stock <= StockMax && params.includes(item.brand)  ||  params.includes(item.category))
+        productsContainer.innerHTML = (extraCards.map(item => Card(item))).join("");
+    } else if(Brand.length == 0 && Category.length == 0) {
+        extraCardsAllFilter = arr1.filter(item => item.price >= NumberMin && item.price <= NumberMax && item.stock >= StockMin && item.stock <= StockMax)
+        productsContainer.innerHTML = (extraCardsAllFilter.map(item => Card(item))).join("");
+    } else  {
+        extraCardsAllFilter = arr1.filter(item => item.price >= NumberMin && item.price <= NumberMax && item.stock >= StockMin && item.stock <= StockMax && params.includes(item.brand)  &&  params.includes(item.category))
+        productsContainer.innerHTML = (extraCardsAllFilter.map(item => Card(item))).join("");
+    }
+
+}
+
+
+function NewFiltArrNumb(arr1, num1, num2) {
+
+        let extraCardsNum = arr1.filter(item => item.price >= num1 && item.price <= num2)
+        productsContainer.innerHTML = (extraCardsNum.map(item => Card(item))).join("");
+
+}
+
+function NewFiltArrStock(arr1, num1, num2) {
+
+    let extraCardsStock = arr1.filter(item => item.stock >= num1 && item.stock <= num2)
+    productsContainer.innerHTML = (extraCardsStock.map(item => Card(item))).join("");
+
+}
