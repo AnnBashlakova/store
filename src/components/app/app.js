6ce698b276c63cf1d54de715a8a87608a62376d7
Anna <anna47297@gmail.com>
2023-01-30 17:48:00 +0300

    feat: add class CartPage

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -8,17 +8,37 @@ import mainPageStore from "../view/pages/mainPageStore"
 export default class App {
     
     constructor() {
-        this.mainSection = document.querySelector('.large-section')
-        this.loadStore()
-        this.store = new mainPageStore
-        this.basket = new Cart();
+        this.mainSection = document.querySelector('.large-section');
+        this.BtnOpenMainPage = document.querySelector('.sneaker-icon-link');
+        this.BtnOpenBasket = document.querySelector('.basket-icon-link')
+        this.loadStore();
+        
+
+        // this.BtnOpenMainPage.addEventListener('click', this.loadStore()) 
+
+        // this.BtnOpenBasket.addEventListener('click', this.loadBasket()) 
+
         
         
     }
         loadStore() {
-            const mainPage = new mainPageStore()
-            this.mainSection.innerHTML = mainPage.render();
+            console.log('append')
+            
+            const mainPage = new mainPageStore();
+            console.log((mainPage.render()))
+
+            
+            this.mainSection.appendChild(mainPage.render());
         }
+
+
+        loadBasket() {
+            console.log('basket')
+            this.mainSection.innerHTML =''
+            const basketPage = new Cart();
+            this.mainSection.appendChild(basketPage.onLoadBasket())
+        }
+
 
         // openBasket() {
         //     this.basket = new Cart();
